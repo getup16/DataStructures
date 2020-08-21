2cf24c1db3ac09e0b0a8962e8e0516e3b4a6a7b8
Anmol Gupta <anmolgupta520@gmail.com>
2020-08-21 16:13:15 +0530

    Corrected PolynomilaLinkedList.cpp (add function left)

--- Practicals/PolynomialLinkedList/poly.cpp
+++ Practicals/PolynomialLinkedList/poly.cpp
@@ -3,65 +3,21 @@
 #include <iomanip>
 using namespace std;
 
-//Polynomial Class
-template <typename T >
-class Polynomial
-{
-public:
-    T *polynomial;  //to save the polynomial in a array
-    int n;  //highest power of the Polynomial
-    //constructor
-    Polynomial(int n)
-    {
-        n = n + 1;
-        cout<<"Enter the Constants : \n";
-        polynomial = new T[n];
-        for(int i = 0; i < n; ++i)
-        {
-            cout<<"x^"<<i<<" : ";
-            cin>>polynomial[i];
-        }
-    }
-    ~Polynomial()
-    {
-        delete[] polynomial;
-    }
-};
 
 //Polynomial Node Class
 template <typename T>
 class PolynomialNode
 {
 public:
-    Polynomial<T> *polynomialObject;
     PolynomialNode *nextNode; 
-    int largest_power;
+    int power;
+    T cofficient;
     //constructor
-    PolynomialNode(int largest_power = 0, Polynomial<T> *polynomialObject = 0, PolynomialNode *nextNode = 0)
+    PolynomialNode(T power = 0, T cofficient = 0, PolynomialNode *nextNode = 0)
     {   
-        this->polynomialObject = polynomialObject;
-        this->largest_power = largest_power;
+        this->power = power;
+        this->cofficient = cofficient;
         this->nextNode = nextNode;
-    }
-    //destructor
-    ~PolynomialNode()
-    {
-        delete polynomialObject;
-    }
-    //print the node
-    void printPolynomial()
-    {
-        T *temp = polynomialObject->polynomial;
-        for(int i = 0; i < largest_power + 1; i++)
-        {
-            cout<<temp[i]<<"*x^"<<i;
-            if(i < (largest_power))
-            {
-                cout<<" + ";
-            }
-        }
-        cout<<" = 0 \n";
-        temp = 0;
     }
     
 };
@@ -72,7 +28,6 @@ class PolynomialLinkedList
 private:
     //data members
     PolynomialNode<T> *head,*tail,*temp;
-    Polynomial<T> *tempPolynomial;
     int totalNodes;
 public:
     //constructor
@@ -98,62 +53,152 @@ public:
     //member functions
 
     //traverse function
-    void traverse()
+    void print()
     {
         if(head == 0)
-    {
-        cout<<"EMPTY.\n";
-    }
-    else
-    {
-        temp = head;
-        while(temp != 0)
         {
-            temp->printPolynomial();
-            temp = temp->nextNode;
+            cout<<"EMPTY.\n";
         }
-        temp = 0;
+        else
+        {
+            temp = head;
+            while(temp != 0)
+            {
+                cout<<temp->cofficient<<"x^"<<temp->power;
+                temp = temp->nextNode;
+                if(temp != 0)
+                    cout<<" + ";
+            }
+            cout<<"\n";
+            temp = 0;
+        }
     }
-    }
+
     //insert function
-    void insert()
-    {
-        int n;
-        cout<<"Enter the Highest Power of the Polynomial : ";
-        cin>>n;
-        tempPolynomial = new Polynomial<T>(n);
-        temp = new PolynomialNode<T>(n,tempPolynomial);
-        if(head != 0)
+    void insertTerm()
+    {   
+        temp = new PolynomialNode<T>();
+        cout<<"Enter the Power of term : "; cin>>temp->power;
+        cout<<"Enter the Cofficient of term : "; cin>>temp->cofficient;
+        if(head == 0)
+        {
+            head = tail = temp;
+        }
+        else if (temp->power == head->power)
+        {
+            head->cofficient += temp->cofficient;
+        }
+        else if(temp->power > tail->power)
         {
             tail->nextNode = temp;
             tail = temp;
         }
+        else if(temp->power < head->power)
+        {
+            temp->nextNode = head;
+            head = temp;
+        }
         else
         {
-            head = tail = temp;
+            PolynomialNode<T>* requiredNode = head;
+            while(requiredNode != 0)
+            {
+                if(requiredNode->nextNode->power > temp->power)
+                {
+                    temp->nextNode = requiredNode->nextNode;
+                    requiredNode->nextNode = temp;
+                    break;
+                }
+                else if(requiredNode->nextNode->power == temp->power)
+                {
+                    requiredNode->nextNode->cofficient += temp->cofficient;
+                    break;
+                }
+                requiredNode = requiredNode->nextNode;
+            }
+            requiredNode = 0;
         }
-        tempPolynomial = 0;
         temp = 0;
         totalNodes++;
     }
-
+    //delete a term in the selected polynomial
+    void deleteTerm()
+    {
+        int loc;
+        cout<<"Enter the location you want to remove : "; cin>>loc;
+        if(loc < 0 || loc > totalNodes)
+        {
+            cout<<"Invalid Location\n";
+        }
+        else if(loc == 1)
+        {
+            temp = head;
+            if(totalNodes == 1)
+            {
+                head = tail = 0;
+            }
+            else
+            {
+                head = head->nextNode;
+            }
+            delete temp;
+            temp = 0;
+            totalNodes--;            
+        }
+        else
+        {
+            int counter = 1;
+            PolynomialNode<T> *requiredNode = head;
+            while(counter != (loc-1))
+            {
+                requiredNode= requiredNode->nextNode;
+                counter++;
+            }
+            temp = requiredNode->nextNode;
+            requiredNode->nextNode = temp->nextNode;
+            //checking if the entered location is a tail
+            if(temp == tail)
+            {
+                tail = requiredNode;
+                requiredNode->nextNode = 0;
+            }
+            delete temp;
+            temp = 0;
+            totalNodes--;
+            
+        }
+    }
+    //add two polynomial
+    void add()
+    {
+        
+    }
 };
 
 
 int main()
 {
 
-    PolynomialLinkedList<int> *polyList = new PolynomialLinkedList<int>();
-        char flag = 'y';
+    PolynomialLinkedList<int> *poly;
+    PolynomialLinkedList<int> *poly_1 = new PolynomialLinkedList<int>();
+    PolynomialLinkedList<int> *poly_2 = new PolynomialLinkedList<int>();
+    int currentPolynomial = 1;
+    poly = poly_1;
+    char flag = 'y';
     int choice;
     //menu
     do{
         cout<<"\n";
         cout<<setw(20)<<setfill(' ')<<"POLYNOMIAL LINKED LIST\n";
         cout<<setw(30)<<setfill('-')<<"\n";
-        polyList->traverse();
+        cout<<"Polynomial 1 : "; poly_1->print();
+        cout<<"Polynomial 2 : "; poly_2->print();
         cout<<setw(30)<<setfill('-')<<"\n";
-        cout<<"1. Insert \n"
+        cout<<"CURRENTLY SELECT : "<<currentPolynomial<<"\n";
+        cout<<setw(30)<<setfill('-')<<"\n";
+        cout<<"1. Switch \n"
+            <<"2. Insert \n"
+            <<"3. Delete \n"
             <<"13. Exit\n";
         //getting user input
         cout<<"Enter What you want to do ? : ";
@@ -162,7 +207,14 @@ int main()
         switch (choice)
         {
             case 1:
-                polyList->insert();
+                currentPolynomial = (currentPolynomial == 1) ? 2 : 1;
+                poly = (poly == poly_1) ? poly_2 : poly_1;
+                break;
+            case 2:
+                poly->insertTerm();
+                break;
+            case 3:
+                poly->deleteTerm();
                 break;
             case 13:
                 flag = 'n';
@@ -174,6 +226,7 @@ int main()
     while(flag == 'y');
     cout<<"\n";
     system("CLS");
-    delete polyList;
+    delete poly_1;
+    delete poly_2;
     return 0;
 }
